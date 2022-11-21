9618d9cc2c9ebfc4f287b2c9e6ce330cc9dbb73f
Manuel Solano <msolanos@gmail.com>
2022-11-21 15:20:26 -0600

    Hello Reasteasy project

---
+++ src/test/java/org/acme/hello/RESTControllerIT.java
@@ -0,0 +1,8 @@
+package org.acme.hello;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class RESTControllerIT extends RESTControllerTest {
+    // Execute the same tests but in packaged mode.
+}
